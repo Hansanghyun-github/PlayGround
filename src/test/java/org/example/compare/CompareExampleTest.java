90c1b8a9eceacf8ef5b1b1c7be9c79039784dbed
HSH <han41562@gmail.com>
2024-11-30 11:18:20 +0900

    study sort with Comparator

--- src/test/java/org/example/compare/CompareExampleTest.java
+++ src/test/java/org/example/compare/CompareExampleTest.java
@@ -3,6 +3,9 @@ package org.example.compare;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -50,5 +53,29 @@ class CompareExampleTest {
         // then
         assertThat(result).isZero();
         assertThat(compareTo).isZero();
+    }
+
+    @Test
+    void 오름차순_정렬() throws Exception {
+        // given
+        List<Integer> list = new ArrayList<>(List.of(10, 5, 20, 15, 25));
+
+        // when
+        list.sort((a, b) -> a - b);
+
+        // then
+        assertThat(list).containsExactly(5, 10, 15, 20, 25);
+    }
+
+    @Test
+    void 내림차순_정렬() throws Exception {
+        // given
+        List<Integer> list = new ArrayList<>(List.of(10, 5, 20, 15, 25));
+
+        // when
+        list.sort((a, b) -> b - a);
+
+        // then
+        assertThat(list).containsExactly(25, 20, 15, 10, 5);
     }
 }
