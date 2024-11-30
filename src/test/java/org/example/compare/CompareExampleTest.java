e59005ec95f571ecc1fffbb1d5fd829acb1e29c2
HSH <han41562@gmail.com>
2024-11-30 11:19:46 +0900

    study sort with Comparator method chainning

--- src/test/java/org/example/compare/CompareExampleTest.java
+++ src/test/java/org/example/compare/CompareExampleTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -74,6 +75,30 @@ class CompareExampleTest {
 
         // when
         list.sort((a, b) -> b - a);
+
+        // then
+        assertThat(list).containsExactly(25, 20, 15, 10, 5);
+    }
+
+    @Test
+    void Comparator_이용하여_오름차순_정렬() throws Exception {
+        // given
+        List<Integer> list = new ArrayList<>(List.of(10, 5, 20, 15, 25));
+
+        // when
+        list.sort(Comparator.comparing(Integer::intValue));
+
+        // then
+        assertThat(list).containsExactly(5, 10, 15, 20, 25);
+    }
+
+    @Test
+    void Comparator_이용하여_내림차순_정렬() throws Exception {
+        // given
+        List<Integer> list = new ArrayList<>(List.of(10, 5, 20, 15, 25));
+
+        // when
+        list.sort(Comparator.comparing(Integer::intValue).reversed());
 
         // then
         assertThat(list).containsExactly(25, 20, 15, 10, 5);
